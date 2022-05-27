0ecbf8290cb48400895c278a9b969d54f0975acd
Tyler Haden <tylerjohnhaden@gmail.com>
2022-05-27 13:23:09 -0400

    add port param

--- src/gstream_to_ros.py
+++ src/gstream_to_ros.py
@@ -5,12 +5,15 @@ from cv_bridge import CvBridge, CvBridgeError
 import rospy
 from sensor_msgs.msg import Image
 
+rov_camera_port = rospy.get_param('/rov_camera_port')
+gstreamer_options = f'udpsrc port={rov_camera_port} ! application/x-rtp, payload=96 ! rtph264depay ! avdec_h264 ! videoconvert ! appsink'
+# terminal version: gst-launch-1.0 -e -v udpsrc port=5600 ! application/x-rtp, payload=96 ! rtph264depay ! avdec_h264 ! autovideosink
+
 rospy.init_node('gstream_to_ros_node')
 pub = rospy.Publisher('/blue_rov_camera', Image, queue_size=5)
 bridge = CvBridge()
 
-# terminal version: gst-launch-1.0 -e -v udpsrc port=5600 ! application/x-rtp, payload=96 ! rtph264depay ! avdec_h264 ! autovideosink
-cap = cv2.VideoCapture('udpsrc port=5600 ! application/x-rtp, payload=96 ! rtph264depay ! avdec_h264 ! videoconvert ! appsink')
+cap = cv2.VideoCapture(gstreamer_options)
 
 ret, frame = cap.read()
 while ret and not rospy.is_shutdown():
